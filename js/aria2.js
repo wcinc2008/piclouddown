377ca01334c64e6f0e50bdd2d85eb3046c4cc6dd
wzhy90 <wzhy90@qq.com>
2013-06-10 23:52:23 +0800

    汉化检漏2

--- js/aria2.js
+++ js/aria2.js
@@ -721,7 +721,7 @@ if (typeof ARIA2=="undefined"||!ARIA2) var ARIA2=(function(){
           var title = "↓"+YAAW.tpl.view.format_size_0()(result.downloadSpeed);
           if (result.uploadSpeed > 0)
             title += " ↑"+YAAW.tpl.view.format_size_0()(result.uploadSpeed);
-          title += " - Yet Another Aria2 Web Frontend";
+          title += " - Aria2管理器";
           document.title = title;
         }
       );
